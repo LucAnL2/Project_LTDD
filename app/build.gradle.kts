8db31b55a47370cf2394032bc743ab6e400a028e
“LucAnL2” <huynhthaitoan.12c1htk@gmail.com>
2025-01-22 23:30:37 +0700

    them giao dien dang nhap

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.project_ltdd"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.project_ltdd"
